1f726be611d8050451ebb0746c3fd47881fedc9d
Carlos Augusto Diniz Filho <ysneshy@hotmail.com>
2022-11-15 09:03:17 -0300

    upgrade33 :zap:

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -12,8 +12,8 @@ const ContactPage = () => {
       <div className={styles.container}>
         <div>
           <h3 className={styles.heading}><FaUserAstronaut />&nbsp;Encontre-me nas redes sociais|</h3>
-          <div className={styles.Image}>
-            <img src="astrocontact.svg" width="300px"  alt="software" />
+          <div className={styles.illustration}>
+            <img src="astrocontact.svg" width="220px"  alt="software" />
           </div>
           <ContactCode />
           <ButtonCV />
