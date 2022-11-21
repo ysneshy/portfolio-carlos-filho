0027b5604cc40cb6f57d5203367a68f466a47f37
Carlos Augusto Diniz Filho <ysneshy@hotmail.com>
2022-11-20 22:42:37 -0300

    constelacao :closed_lock_with_key::bug::zap::rocket:

--- components/Orion.jsx
+++ components/Orion.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/Estelar.module.css'
 
 const AkiraYXK = () => {
     return (
-        <div className={styles.container}>
+        <div className={styles.galaxia}>
             <div className={styles.stars}>
                 <div className={styles.stars2}></div>
             </div>
