a8cfa256eac96596a1dca714cf27b2b4f4708ea5
Carlos Augusto Diniz Filho <ysneshy@hotmail.com>
2022-12-04 11:18:34 -0300

    Upgrade993 :closed_lock_with_key::bug::zap::rocket:

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -18,8 +18,6 @@ const Layout = ({ children }) => {
           <div className={styles.stars3}></div>
           <div className={styles.shooting_star}></div>
           <div className={styles.shooting_star}></div>
-          <div className={styles.shooting_star}></div>
-          <div className={styles.shooting_star}></div>
           <Tabsbar />
           <main className={styles.content}>{children}</main>
         </div>
