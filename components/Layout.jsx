2d7a7d31238289106836a55620672b830bdc9e6a
ysneshy <ysneshy@hotmail.com>
2022-12-16 11:31:38 -0300

    :zap:1002 upgrade

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -22,8 +22,6 @@ const Layout = ({ children }) => {
           <div className={styles.terra}></div>
           <ul><li><p className={styles.sunshine}></p></li></ul>
           <ul><li className={styles.sun}></li></ul>
-          <div className={styles.pulse}>
-          </div>
           
           <Tabsbar />
           <main className={styles.content}>{children}</main>
