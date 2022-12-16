030e7a9874d853afb78d73f4fdbb1727524eb415
ysneshy <ysneshy@hotmail.com>
2022-12-16 11:42:53 -0300

    :zap:1002 upgrade

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,9 +14,6 @@ const ContactPage = () => {
         <div>
           <h3 className={styles.heading}><FaUserAstronaut />&nbsp;Encontre-me nas Redes Sociais.</h3>
           <div className={styles.illustration}>
-          <Rotate Rotate delay={1000} count={3}>
-            <img src="astrocontact.svg" width="160px"  alt="software" />
-            </Rotate>
           </div>
           <ContactCode />
           <ButtonCV />
