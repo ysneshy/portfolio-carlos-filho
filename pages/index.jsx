733db2824a96486b693da3cff78d7e9a94dad957
ysneshy <ysneshy@hotmail.com>
2022-12-19 12:40:55 -0300

    upgrade

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,6 @@ import styles from "../styles/HomePage.module.css";
 import Loading from "../components/Loading";
 import { FaUserAstronaut } from "react-icons/fa";
 import { FaMailchimp } from "react-icons/fa";
-import { Fade, Rotate, Zoom, Swing, Flip } from "react-reveal";
 import { DiAndroid, DiMsqlServer } from "react-icons/di";
 
 export default function HomePage() {
@@ -27,16 +26,15 @@ export default function HomePage() {
           <br></br>
           <br></br>
           <br></br>
+          
           <div className={styles.foreground}>
             <div className={styles.content}>
-              <Flip delay={1000} count={3}>
                 <h6 className={styles.name}>
                   <span>
                     <FaUserAstronaut />
                   </span>
                   &nbsp;Carlos Augusto Diniz Filho
                 </h6>
-              </Flip>
               <h6 className={styles.bio}>Desenvolvedor Front-End</h6>
               <Link href="/projects">
                 <button className={styles.button}>
